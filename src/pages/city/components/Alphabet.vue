ff2cc445f62e551e2f240ac3749f706bf9b2a2e8
yuxinchan <837330534@qq.com>
2019-09-23 15:34:14 +0800

    初步实现城市页面

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  .list
+    display flex
+    flex-direction column
+    justify-content center
+    position absolute
+    top 1.58rem
+    right 0
+    bottom 0
+    width .4rem
+    .item
+      list-style none
+      line-height .4rem
+      text-align center
+      color $bgColor
+</style>
