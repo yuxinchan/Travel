52fc00dc18e86698173c18f9c8c40ff4109a085b
yuxinchan <837330534@qq.com>
2019-09-23 11:05:33 +0800

    city router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe640;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    position relative;
+    overflow hidden
+    height $headerHeight
+    line-height $headerHeight
+    text-align center
+    color #fff
+    font-size .32rem
+    background $bgColor
+    .header-back
+      position absolute
+      top 0
+      left 0
+      padding-left .1rem
+      width .64rem
+      text-align center
+      font-size .6rem
+      color #fff
+</style>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -58,6 +58,6 @@ export default {
 }
 </script>
 
-<style>
+<style lang="stylus" scoped>
 
 </style>
