386f462ab908c567a7a30d09d80e1a98c5a63cef
yuxinchan <837330534@qq.com>
2019-09-24 17:09:50 +0800

    实现页面的异步请求

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../pages/home/Home'
 import City from '../pages/city/City'
-import Detail from '../pages/detail/Detail'
 
 Vue.use(Router)
 
@@ -18,7 +17,7 @@ export default new Router({
   }, {
     path: '/detail/:id',
     name: 'Detail',
-    component: Detail
+    component: () => import('../pages/detail/Detail') // 异步加载，当项目较庞大的时候使用
   }],
   scrollBehavior (to, from, savedPosition) {
     return { x: 0, y: 0 }
